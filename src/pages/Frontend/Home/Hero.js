143fda48334bc3cc78b3d948653fb577214daf02
Hasnat Majid <107733484+HasnatGill@users.noreply.github.com>
2023-06-17 19:33:58 +0500

    Some Tweaks

--- src/pages/Frontend/Home/Hero.js
+++ src/pages/Frontend/Home/Hero.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import WebViewer from '@pdftron/webviewer'
 
 export default function Hero() {
-  return (
-    <div>
-        <h1 className='text-center'>This is Value Get Pdf File</h1>
-    </div>
-  )
+
+    const viewer = useRef(null);
+
+    useEffect(() => {
+        WebViewer(
+            {
+                path: '/lib',
+                // licenseKey: 'YOUR_LICENSE_KEY',
+                initialDoc: 'https://pdftron.s3.amazonaws.com/downloads/pl/demo-annotated.pdf',
+            },
+            viewer.current,
+        ).then((instance) => {
+            const { documentViewer } = instance.Core;
+            // you can now call WebViewer APIs here...
+        });
+    }, []);
+
+    return (
+        <div>
+            <div className="MyComponent">   
+                <div className="header">React sample</div>
+                <div className="webviewer" ref={viewer} style={{ height: "100vh" }}></div>
+            </div>
+        </div>
+    )
 }

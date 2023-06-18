6e0442603019ed0861f72d1b18e0c759db1b5875
Hasnat Majid <107733484+HasnatGill@users.noreply.github.com>
2023-06-18 11:56:21 +0500

    Tweaks

--- src/pages/Frontend/Home/Hero.js
+++ src/pages/Frontend/Home/Hero.js
@@ -3,18 +3,18 @@ import WebViewer from '@pdftron/webviewer';
 
 export default function Hero() {
 
-    const viewer = useRef(null);
+    // const viewer = useRef(null);
 
     // if using a class, equivalent of componentDidMount 
-    useEffect(() => {
-      WebViewer(
-        {
-            path: '/node_modules/@pdftron/webviewer/lib',
-            licenseKey: 'YOURT_LICENSE_KEY',
-            initialDoc: 'https://pdftron.s3.amazonaws.com/downloads/pl/demo-annotated.pdf',
-        },
-        viewer.current,
-      ).then((instance) => {
+    // useEffect(() => {
+    //   WebViewer(
+    //     {
+    //         path: '/node_modules/@pdftron/webviewer/lib',
+    //         licenseKey: 'YOURT_LICENSE_KEY',
+    //         initialDoc: 'https://pdftron.s3.amazonaws.com/downloads/pl/demo-annotated.pdf',
+    //     },
+    //     viewer.current,
+    //   ).then((instance) => {
         // const { documentViewer, annotationManager, Annotations } = instance.Core;
   
         // documentViewer.addEventListener('documentLoaded', () => {
@@ -32,15 +32,15 @@ export default function Hero() {
         //   // need to draw the annotation otherwise it won't show up until the page is refreshed
         //   annotationManager.redrawAnnotation(rectangleAnnot);
         // });
-      });
-    }, []);
+    //   });
+    // }, []);
   
 
     return (
         <div>
             <div className="MyComponent">   
                 <div className="header">React sample</div>
-                <div className="webviewerOne" ref={viewer} style={{minHeight:"100vh"}}></div>
+                {/* <div className="webviewerOne" ref={viewer} style={{minHeight:"100vh"}}></div> */}
             </div>
         </div>
     )
